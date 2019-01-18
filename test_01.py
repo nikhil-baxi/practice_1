8b10b3fed4d339f9dae24bea5af710ab34262d3d
nikhil-b <nikhil-b@mpcfilm.com>
2019-01-18 17:05:13 +0530

    minor change for assert when fails

--- test_01.py
+++ test_01.py
@@ -2,18 +2,25 @@
 # to run the test:- pytest -s -q
 # johnnydeez
 # vim
-
+import pytest
 from script import publicRepos
+import json
 
 def test_01():
     print(".........Test_01.............")
     name = raw_input("enter your username:")
     repoName = raw_input("Enter the repos name to test for: ")
     print("Test is searching for "+repoName+"...")
+    
     data = publicRepos(name)
-    for d in data:
-        for key, value in d.iteritems():
-            if key == "name":
-                if value == repoName:
-                    print("found "+ repoName)
-                    assert True
+    
+    status = False      #status set to true if repo is found
+    
+    for rep in data:
+        if rep.has_key('name'):
+            if rep['name'] == repoName:
+                status = True
+                print ("found " + rep['name'])
+                break
+        
+    assert status == True   
